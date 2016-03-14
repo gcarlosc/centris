b96fdf0ea39247086652cdf2c9e1656d182da968
Jorge Crisóstomo Palacios <videmor@gmail.com>
2016-03-14 12:46:11 -0500

    generate schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160311170620) do
+ActiveRecord::Schema.define(version: 20160224173758) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "accounts", force: :cascade do |t|
-    t.string   "organization_name"
-    t.string   "web"
-    t.string   "subdomain"
-    t.datetime "created_at",        null: false
-    t.datetime "updated_at",        null: false
-  end
 
   create_table "category_products", force: :cascade do |t|
     t.string   "name"
@@ -44,38 +36,12 @@ ActiveRecord::Schema.define(version: 20160311170620) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "items", force: :cascade do |t|
-    t.integer  "sku_id"
-    t.integer  "movement_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
-  add_index "items", ["movement_id"], name: "index_items_on_movement_id", using: :btree
-  add_index "items", ["sku_id"], name: "index_items_on_sku_id", using: :btree
-
   create_table "movement_types", force: :cascade do |t|
     t.string   "name"
     t.string   "factor"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-  create_table "movements", force: :cascade do |t|
-    t.integer  "creator_id"
-    t.integer  "responsable_id"
-    t.integer  "movement_type_id"
-    t.integer  "originable_id"
-    t.string   "originable_type"
-    t.integer  "destinable_id"
-    t.string   "destinable_type"
-    t.datetime "created_at",                   null: false
-    t.datetime "updated_at",                   null: false
-    t.integer  "status",           default: 0
-  end
-
-  add_index "movements", ["destinable_type", "destinable_id"], name: "index_movements_on_destinable_type_and_destinable_id", using: :btree
-  add_index "movements", ["originable_type", "originable_id"], name: "index_movements_on_originable_type_and_originable_id", using: :btree
 
   create_table "organizations", force: :cascade do |t|
     t.string   "name"
@@ -89,12 +55,11 @@ ActiveRecord::Schema.define(version: 20160311170620) do
   create_table "products", force: :cascade do |t|
     t.string   "name"
     t.integer  "category_product_id"
-    t.integer  "classification_id"
     t.integer  "description_id"
+    t.integer  "classification_id"
     t.integer  "unit_id"
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
-    t.string   "code"
   end
 
   create_table "projects", force: :cascade do |t|
@@ -103,30 +68,13 @@ ActiveRecord::Schema.define(version: 20160311170620) do
     t.integer  "organization_id"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-    t.string   "code"
   end
-
-  create_table "skus", force: :cascade do |t|
-    t.string   "sku"
-    t.integer  "product_id"
-    t.integer  "warehouse_id"
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
-    t.integer  "status",       default: 0
-  end
-
-  add_index "skus", ["product_id"], name: "index_skus_on_product_id", using: :btree
-  add_index "skus", ["sku"], name: "index_skus_on_sku", unique: true, using: :btree
-  add_index "skus", ["warehouse_id"], name: "index_skus_on_warehouse_id", using: :btree
 
   create_table "suppliers", force: :cascade do |t|
     t.string   "name"
     t.string   "address"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-    t.string   "contact_name"
-    t.string   "email"
-    t.integer  "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "units", force: :cascade do |t|
@@ -139,13 +87,11 @@ ActiveRecord::Schema.define(version: 20160311170620) do
     t.string   "email"
     t.integer  "phone"
     t.string   "type"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.string   "password_digest"
-    t.string   "fullname"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "first_name"
+    t.string   "last_name"
   end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
   create_table "warehouses", force: :cascade do |t|
     t.string   "name"
@@ -154,8 +100,6 @@ ActiveRecord::Schema.define(version: 20160311170620) do
     t.integer  "project_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.float    "longitude"
-    t.float    "latitude"
   end
 
 end
